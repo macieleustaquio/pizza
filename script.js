8b5daa2c4e776a356fb4946bb8c3358f7adbc671
Maciel Eustaquio <maciel.seo@gmail.com>
2022-08-09 08:15:11 -0300

    Finaliza a parte 3 do projeto.

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const c = (el) => {
-    return document.querySelector(el);
-}
-
-const cs = (el) => {
-    return document.querySelector(el);
-}
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelector(el);
 
 pizzaJson.map((item, index)=> {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     //preencher as informações da pizza
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
     c('.pizza-area').append(pizzaItem);
 
