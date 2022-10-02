61385f543d0e95baebc9106150265b08a6192c10
Maciel Eustaquio <maciel.seo@gmail.com>
2022-10-02 18:06:41 -0300

    Finaliza a parte 12 do projeto.

--- script.js
+++ script.js
@@ -102,11 +102,36 @@ c(".pizzaInfo--addButton").addEventListener("click", () => {
 function updateCart() {
   if(cart.length > 0) {
     c('aside').classList.add('show');
+    c('.cart').innerHTML = '';
+
     for(let i in cart) {
       let pizzaItem = pizzaJson.find((item)=>{
         return item.id == cart[i].id;
       });
-      console.log(pizzaItem);
+      
+      let cartItem = c('.models .cart--item').cloneNode(true);
+
+      let pizzaSizeName;
+      switch(cart[i].size) {
+        case 0:
+          pizzaSizeName = 'P'
+          break;
+        case 1:
+          pizzaSizeName = 'M'
+          break;
+        case 2:
+          pizzaSizeName = 'G'
+          break;
+      }
+      
+      let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+      cartItem.querySelector('img').src = pizzaItem.img;
+      cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+      cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qtd;
+
+      c('.cart').append(cartItem);
+
     }
 
   } else {
