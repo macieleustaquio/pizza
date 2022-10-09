8336846b08bd9867ae2ef8732b4cc9a569505d44
Maciel Eustaquio <maciel.seo@gmail.com>
2022-10-09 13:40:27 -0300

    [Finaliza o projeto pizza.]

--- script.js
+++ script.js
@@ -99,7 +99,18 @@ c(".pizzaInfo--addButton").addEventListener("click", () => {
   closeModal();
 });
 
+c('.menu-openner').addEventListener('click', () => {
+  if(cart.length > 0) {
+    c('aside').style.left = '0';
+  }
+});
+
+c('.menu-closer').addEventListener('click', () => {
+  c('aside').style.left = '100vw';
+});
+
 function updateCart() {
+  c('.menu-openner span').innerHTML = cart.length;
   if(cart.length > 0) {
     c('aside').classList.add('show');
     c('.cart').innerHTML = '';
@@ -157,5 +168,7 @@ function updateCart() {
 
   } else {
     c('aside').classList.remove('show');
+    c('aside').classList.remove('show');
+    c('aside').style.left = '100vw';
   }
 }
